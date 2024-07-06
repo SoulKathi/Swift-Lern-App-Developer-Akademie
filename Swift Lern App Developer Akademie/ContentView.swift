73183bee3431599129700fffbe619847c26fadee
Katja Rübel <Katja.ruebel@gmail.com>
2024-07-07 01:56:29 +0200

    Initial Commit

---
+++ Swift Lern App Developer Akademie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift Lern App Developer Akademie
+//
+//  Created by katja.ruebel on 07.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift Lern App Developer Akademie/Swift_Lern_App_Developer_AkademieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Lern_App_Developer_AkademieApp.swift
+//  Swift Lern App Developer Akademie
+//
+//  Created by katja.ruebel on 07.07.24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Lern_App_Developer_AkademieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
